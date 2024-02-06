dd308e60725922dad3c293818d3ccc90dc65dd4f
mathieu <mathieu.dugre@mdugre.info>
2024-02-06 13:12:28 -0500

    Env developpement

--- src/ConfigurerAppareil.js
+++ src/ConfigurerAppareil.js
@@ -1,5 +1,28 @@
+import {useState, useCallback, useEffect} from 'react'
+
+import Button from 'react-bootstrap/Button'
+
 function ConfigurerAppareil(props) {
-    return 'Configurer'
+    return (
+        <div>
+            <AppareilsPaired />
+        </div>
+    )
 }
 
 export default ConfigurerAppareil
+
+function AppareilsPaired(props) {
+
+    const ajouterCb = useCallback(()=>{
+        console.debug("Request device")
+    }, [])
+
+    return (
+        <div>
+            <p>Appareils connus</p>
+
+            <p><Button variant="secondary" onClick={ajouterCb}>Ajouter</Button></p>
+        </div>
+    )
+}
